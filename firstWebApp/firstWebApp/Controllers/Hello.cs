d98ba7bab0036a0c3b76039e37cb03aa73d2928f
sravanthichava <chavasravanthi1@gmail.com>
2021-07-28 17:54:16 -0700

    mycommit

---
+++ firstWebApp/firstWebApp/Controllers/Hello.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sravanthi : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+        [HttpGet]
+        [Route("myapplication")]
+        public IActionResult GetMyApplication()
+        {
+            return Ok("Hello World Application");
+        }
+        [HttpPost]
+        public IActionResult post(JObject payload )
+        {
+            return Ok(payload);
+
+        }
+    }
+}
